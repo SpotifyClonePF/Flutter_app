b039453b81a6f4e8dfd7cd3b639b2e4661871433
RubenSans <rubensanbel@campus.monlau.com>
2023-03-02 16:27:49 +0100

    Añadido main y home

---
+++ lib/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blueGrey.shade300,
+                Colors.black,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        )
+    );
+  }
+}
