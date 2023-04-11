1b52f6f413e25dd00b7b58779479d1c7a395ab27
RubenSans <rubensanbel@campus.monlau.com>
2023-04-11 17:56:31 +0200

    Test search mobile

--- lib/screens/mobile/tabs/search_page.dart
+++ lib/screens/mobile/tabs/search_page.dart
@@ -10,183 +10,113 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-
   String _searchText = '';
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 30),
-        child: Scaffold(
+      padding: const EdgeInsets.all(15),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
           backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            title: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                hintStyle: const TextStyle(color: Colors.white),
-                prefixIcon: const Icon(
-                  Icons.search,
-                  color: Colors.white,
-                ),
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.2),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(vertical: 8),
+          elevation: 0,
+          toolbarHeight: 80,
+          title: TextField(
+            decoration: InputDecoration(
+              hintText: 'Search',
+              hintStyle: const TextStyle(color: Colors.white),
+              prefixIcon: const Icon(
+                Icons.search,
+                color: Colors.white,
               ),
-              textAlignVertical: TextAlignVertical.center,
-              style: const TextStyle(color: Colors.white),
-              onChanged: (value) {
-                setState(() {
-                  _searchText = value;
-                });
-              },
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.2),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: const EdgeInsets.symmetric(vertical: 8),
             ),
+            textAlignVertical: TextAlignVertical.center,
+            style: const TextStyle(color: Colors.white),
+            onChanged: (value) {
+              setState(() {
+                _searchText = value;
+              });
+            },
           ),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            physics: const BouncingScrollPhysics(
-                decelerationRate: ScrollDecelerationRate.fast),
-            child: Column(
-              children: [
-                /// Las mas reproducidas esta semana
-                Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 15),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          'Las más reproducidas \nesta semana',
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          physics: const BouncingScrollPhysics(
+              decelerationRate: ScrollDecelerationRate.fast),
+          child: Column(
+            children: [
+              /// Busquedas recientes
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Recent searches',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 30,
+                            fontSize: 16,
                             fontWeight: FontWeight.w800,
                           ),
                         ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                      child: Table(
-                          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                          columnWidths: const {
-                            0: FixedColumnWidth(45),
-                            1: FlexColumnWidth(),
-                            2: FixedColumnWidth(60),
-                            3: FixedColumnWidth(32),
-                          },
-                          children: List.generate(
-                            5,
-                                (index) {
-                              return TableRow(
-                                children: [
-                                  Text(
-                                    '0${index + 1}',
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 5),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          songs[index]['title'],
-                                          style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                        const SizedBox(height: 5),
-                                        Text(
-                                          songs[index]['artist'],
-                                          style: const TextStyle(
-                                            color: MyColors.mainGreen,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.more_vert,
-                                    color: Colors.white,
-                                  ),
-                                  Text(
-                                    songs[index]['duration'],
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ],
-                              );
-                            },
-                          )
-                      ),
-                    ),
-                  ],
-                ),
-
-                /// ULTIMAS CANCIONES AÑADIDAS
-                Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(top: 20, bottom: 10, left: 15),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          'Últimas canciones \nañadidas',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.w800,
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Clear',
+                            style: TextStyle(
+                              color: MyColors.mainGreen,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w800,
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                    SingleChildScrollView(
-                      physics: const BouncingScrollPhysics(decelerationRate: ScrollDecelerationRate.fast),
-                      scrollDirection: Axis.horizontal,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Row(
-                          children: List.generate(songs.length, (index) {
-                            return GestureDetector(
-                              onTap: () {},
-                              child: SizedBox(
-                                width: 170,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 20),
+                    child: Table(
+                      defaultVerticalAlignment:
+                          TableCellVerticalAlignment.middle,
+                      columnWidths: const {
+                        0: FixedColumnWidth(55),
+                        1: FlexColumnWidth(),
+                        2: FixedColumnWidth(35),
+                      },
+                      children: List.generate(
+                        5,
+                        (index) {
+                          return TableRow(
+                            children: [
+                              Container(
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(songs[index]['img']),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
                                 child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Container(
-                                      width: 140,
-                                      height: 140,
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          image: AssetImage(songs[index]['img']),
-                                          fit: BoxFit.cover,
-                                        ),
-                                        color: Colors.green,
-                                        borderRadius: BorderRadius.circular(15),
-                                      ),
-                                    ),
-                                    const SizedBox(height: 10),
                                     Text(
                                       songs[index]['title'],
                                       style: const TextStyle(
@@ -194,36 +124,43 @@ class _SearchState extends State<Search> {
                                         fontSize: 16,
                                         fontWeight: FontWeight.w600,
                                       ),
-                                      maxLines: 1,
-                                      textAlign: TextAlign.center,
+                                      overflow: TextOverflow.ellipsis,
                                     ),
                                     const SizedBox(height: 5),
-                                    SizedBox(
-                                      width: 180,
-                                      child: Text(
-                                        songs[index]['artist'],
-                                        maxLines: 1,
-                                        textAlign: TextAlign.center,
-                                        style: const TextStyle(
-                                          color: MyColors.mainGreen,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w600,
-                                        ),
+                                    Text(
+                                      songs[index]['artist'],
+                                      style: const TextStyle(
+                                        color: MyColors.mainGreen,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
                                       ),
-                                    )
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
                                   ],
                                 ),
                               ),
-                            );
-                          }),
+                              const Icon(
+                                Icons.more_vert,
+                                color: Colors.white,
+                              ),
+                            ],
+                          );
+                        },
+                      ).expand((row) => [
+                        row,
+                        const TableRow(
+                          children: [
+                            Divider(height: 40, color: Colors.white),
+                            Divider(color: Colors.white),
+                            Divider(color: Colors.white),
+                          ],
                         ),
-                      ),
+                      ]).toList(),
                     ),
-                  ],
-                ),
-                const SizedBox(height: 90),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
