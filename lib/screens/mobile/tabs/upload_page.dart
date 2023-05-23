9dc3346448d6b1ead8c8bf414dc05b4b91dff807
RubenSans <rubensanbel@campus.monlau.com>
2023-05-23 20:42:35 +0200

    23/05/23 upload sin función

--- lib/screens/mobile/tabs/upload_page.dart
+++ lib/screens/mobile/tabs/upload_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
 
 class Upload extends StatefulWidget {
   const Upload({Key? key}) : super(key: key);
@@ -14,13 +15,17 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+
   File? image;
+  PlatformFile? musicFile;
+  String selectedFileName = '', nameSong = '', nameArtist = '';
+
   Future _pickImageFromGallery() async {
     try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (image == null) return;
-      final imageTemporary = File(image.path);
-      setState(() => this.image = imageTemporary);
+      final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (pickedImage == null) return;
+      final imageTemporary = File(pickedImage.path);
+      setState(() => image = imageTemporary);
     } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
@@ -67,98 +72,44 @@ class _UploadState extends State<Upload> {
       barrierColor: Colors.black.withOpacity(0.7),
       builder: (BuildContext context) {
         return BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-          child: AlertDialog(
-            insetPadding: const EdgeInsets.all(0),
-            backgroundColor: MyColors.darkGray,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            title: Align(
-              alignment: Alignment.topLeft,
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    image = null;
-                  });
-                  Navigator.of(context).pop();
-                },
-                child: const Icon(
-                  Icons.close,
-                  color: Colors.white,
-                  size: 30,
-                ),
+            filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+        child: AlertDialog(
+          insetPadding: const EdgeInsets.all(0),
+          backgroundColor: MyColors.darkGray.withOpacity(0.8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          title: Align(
+            alignment: Alignment.topLeft,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  image = null;
+                  musicFile = null;
+                  selectedFileName = '';
+                  nameSong = '';
+                  nameArtist = '';
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Icon(
+                Icons.close,
+                color: Colors.white,
+                size: 30,
               ),
             ),
-            content: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(
-                  decelerationRate: ScrollDecelerationRate.normal),
-              child: ListBody(
-                children: <Widget>[
-                  /// Upload icon
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 2,
-                            ),
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() async{
-                                await _pickImageFromGallery();
-                                Navigator.of(context).pop();
-                                _showMyDialog();
-                              });
-                            },
-                            child: image != null
-                                ? ClipRRect(
-                                    borderRadius: BorderRadius.circular(20),
-                                    child: Image.file(
-                                      image!,
-                                      fit: BoxFit.cover,
-                                      width: 130,
-                                      height: 135,
-                                    ),
-                                  )
-                                : const Padding(
-                                    padding: EdgeInsets.all(40),
-                                    child: Icon(
-                                      Icons.add_photo_alternate,
-                                      color: Colors.white,
-                                      size: 50,
-                                    ),
-                                  ),
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 15),
-                          child: Text("Cover image",
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500)),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  /// Upload song button
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    child: InkWell(
-                      onTap: () {
-                        _dialogFuture();
-                      },
-                      child: Container(
+          ),
+          content: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(
+                decelerationRate: ScrollDecelerationRate.normal),
+            child: ListBody(
+              children: <Widget>[
+                /// Upload icon
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    children: [
+                      Container(
                         decoration: BoxDecoration(
                           shape: BoxShape.rectangle,
                           border: Border.all(
@@ -168,113 +119,203 @@ class _UploadState extends State<Upload> {
                           color: Colors.transparent,
                           borderRadius: BorderRadius.circular(20),
                         ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(7),
-                          child: Icon(
-                            Icons.add_box_outlined,
-                            color: Colors.white,
-                            size: 40,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() async{
+                              await _pickImageFromGallery();
+                              Navigator.of(context).pop();
+                              _showMyDialog();
+                          });
 
-                  /// Song name
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 2,
-                        ),
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Song\'s name',
-                          contentPadding: EdgeInsets.only(left: 20),
-                          hintStyle: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
+                          },
+                          child: image != null
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.file(
+                                    image!,
+                                    fit: BoxFit.cover,
+                                    width: 130,
+                                    height: 135,
+                                  ),
+                                )
+                              : const Padding(
+                                  padding: EdgeInsets.all(40),
+                                  child: Icon(
+                                    Icons.add_photo_alternate,
+                                    color: Colors.white,
+                                    size: 50,
+                                  ),
+                                ),
                         ),
                       ),
-                    ),
-                  ),
-
-                  /// Artist name
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 2,
-                        ),
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Artist\'s name',
-                          contentPadding: EdgeInsets.only(left: 20),
-                          hintStyle: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: Text(
+                            "Cover image",
+                            style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500
+                            )
                         ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.9),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  width: double.infinity,
-                  child: TextButton(
-                    child: const Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Text(
-                        'Upload',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    onPressed: () {
-                      _dialogFuture();
-                    },
+                    ],
                   ),
                 ),
-              ),
-            ],
+
+                /// Upload song button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: InkWell(
+                    onTap: () async {
+                      FilePickerResult? result = await FilePicker.platform.pickFiles(
+                        type: FileType.custom,
+                        allowedExtensions: ['mp3', 'wav'],
+                      );
+                      if (result != null) {
+                        musicFile = result.files.first;
+                        setState(() {
+                          selectedFileName = musicFile!.name; // Variable para almacenar el nombre del archivo seleccionado
+                          Navigator.of(context).pop();
+                          _showMyDialog();
+                        });
+                      }
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(7),
+                        child: selectedFileName != null && selectedFileName.isNotEmpty
+                            ? Text(
+                          selectedFileName!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        )
+                            : const Icon(
+                          Icons.add_box_outlined,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                /// Song name
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child:  TextField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Song\'s name',
+                        contentPadding: EdgeInsets.only(left: 20),
+                        hintStyle: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          nameSong = value;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+
+                /// Artist name
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Artist\'s name',
+                        contentPadding: EdgeInsets.only(left: 20),
+                        hintStyle: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          nameArtist = value;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.9),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                width: double.infinity,
+                child: TextButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      'Upload',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    _dialogFuture();
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
         );
       },
     );
